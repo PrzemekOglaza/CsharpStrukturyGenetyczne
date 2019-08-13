3a0167758f9b7bbf216a132ece75a876744bc8b4
Przemek O <przemyslaw.oglaza@outlook.com>
2019-08-13 15:14:06 +0200

    1_5 Unit Test

---
+++ CsharpStrukturyGeneryczne/1_TypyGeneryczneTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("1_TypyGeneryczneTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("1_TypyGeneryczneTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("04e4cc00-9968-468c-8482-e5e3093b9ce4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
