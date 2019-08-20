58587b64e8cad9c045bbbe46f6dcecb2333abb6f
Przemek O <przemyslaw.oglaza@outlook.com>
2019-08-20 13:41:54 +0200

    2_4 Kolejka

---
+++ CsharpStrukturyGeneryczne/2 Kolekcje Generyczne Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("2 Kolekcje Generyczne Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("2 Kolekcje Generyczne Test")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0ced5c8e-2110-4f74-9d6f-323e150e4e96")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
