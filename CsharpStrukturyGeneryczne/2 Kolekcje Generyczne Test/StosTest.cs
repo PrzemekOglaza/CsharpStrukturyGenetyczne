0d27660099b0bcfa27516a23683769c8a74c9787
Przemek O <przemyslaw.oglaza@outlook.com>
2019-08-21 10:59:52 +0200

    Stos i kolejka + testy

---
+++ CsharpStrukturyGeneryczne/2 Kolekcje Generyczne Test/StosTest.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _2_Kolekcje_Generyczne_Test
+{
+    [TestClass]
+    public class StosTest
+    {
+        [TestMethod]
+        public void UzyciePeek()
+        {
+            var stos = new Stack<int>();
+
+            stos.Push(1);
+            stos.Push(2);
+            stos.Push(3);
+            stos.Push(4);
+           
+            Assert.AreEqual(4, stos.Peek());
+        }
+
+        [TestMethod]
+        public void UzycieContains()
+        {
+            var stos = new Stack<int>();
+
+            stos.Push(1);
+            stos.Push(2);
+            stos.Push(3);
+            stos.Push(4);
+
+            Assert.IsTrue(stos.Contains(3));
+        }
+
+        [TestMethod]
+        public void UzycieTorray()
+        {
+            var stos = new Stack<int>();
+
+            stos.Push(1);
+            stos.Push(2);
+            stos.Push(3);
+            stos.Push(4);
+
+            var tablica = stos.ToArray();
+            stos.Pop();
+
+            Assert.AreEqual(4, tablica[0]);
+            Assert.AreEqual(3, stos.Count);
+        }
+
+
+        [TestMethod]
+        public void UzycieClear()
+        {
+            var stos = new Stack<int>();
+
+            stos.Push(1);
+            stos.Push(2);
+            stos.Push(3);
+            stos.Push(4);
+
+            stos.Clear();
+
+            Assert.AreEqual(0, stos.Count);
+        }
+    }
+}
+
--- CsharpStrukturyGeneryczne/2_KolekcjeGeneryczne/Program.cs
+++ CsharpStrukturyGeneryczne/2_KolekcjeGeneryczne/Program.cs
@@ -10,21 +10,47 @@ namespace _2_KolekcjeGeneryczne
     {
         static void Main(string[] args)
         {
+            Kolejka();
+            Stos();
+
+
+
+
+        }
+
+        private static void Stos()
+        {
+            Stack<Pracownik> stos = new Stack<Pracownik>();
+            stos.Push(new Pracownik { Imie = "Marcin", Nazwisko = "Nowak" });
+            stos.Push(new Pracownik { Imie = "Tomek", Nazwisko = "Kowak" });
+            stos.Push(new Pracownik { Imie = "Jacek", Nazwisko = "Zajac" });
+            stos.Push(new Pracownik { Imie = "Ola", Nazwisko = "Kaczor" });
+
+            Console.WriteLine();
+            Console.WriteLine("Stos");
+
+            while (stos.Count > 0)
+            {
+                var pracownik = stos.Pop();
+                Console.WriteLine(pracownik.Imie + " " + pracownik.Nazwisko);
+            }
+        }
+
+        private static void Kolejka()
+        {
             Queue<Pracownik> kolejka = new Queue<Pracownik>();
             kolejka.Enqueue(new Pracownik { Imie = "Marcin", Nazwisko = "Nowak" });
             kolejka.Enqueue(new Pracownik { Imie = "Tomek", Nazwisko = "Kowak" });
             kolejka.Enqueue(new Pracownik { Imie = "Jacek", Nazwisko = "Zajac" });
             kolejka.Enqueue(new Pracownik { Imie = "Ola", Nazwisko = "Kaczor" });
 
-            
+            Console.WriteLine("Kolejka");
 
             while (kolejka.Count > 0)
             {
                 var pracownik = kolejka.Dequeue();
-                Console.WriteLine(pracownik.Imie +" " + pracownik.Nazwisko);
+                Console.WriteLine(pracownik.Imie + " " + pracownik.Nazwisko);
             }
-
         }
-	
     }
 }
