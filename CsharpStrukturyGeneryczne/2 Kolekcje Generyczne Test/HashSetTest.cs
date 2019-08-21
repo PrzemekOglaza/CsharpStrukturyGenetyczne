0b15aa081cde74a5edab47014c90bff133bc7802
Przemek O <przemyslaw.oglaza@outlook.com>
2019-08-21 11:31:19 +0200

    HashSetTest

---
+++ CsharpStrukturyGeneryczne/2 Kolekcje Generyczne Test/HashSetTest.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _2_Kolekcje_Generyczne_Test
+{
+    [TestClass]
+    public class HashSetTest
+    {
+        [TestMethod]
+        public void CzescWspulnaSetow()
+        {
+            var set1 = new HashSet<int>() { 1, 2, 3 };
+            var set2 = new HashSet<int>() { 2,3,4 };
+
+            set1.IntersectWith(set2);
+
+            Assert.IsTrue(set1.SetEquals(new[] { 2, 3 }));
+        }
+
+        [TestMethod]
+        public void UnionSetWszystkieLiczby()
+        {
+            var set1 = new HashSet<int>() { 1, 2, 3 };
+            var set2 = new HashSet<int>() { 2, 3, 4 };
+
+            set1.UnionWith(set2);
+            Assert.IsTrue(set1.SetEquals(new[] { 1, 2, 3, 4 }));
+        }
+
+        [TestMethod]
+        public void CzescRoznaSymmetricExceptWith()
+        {
+            var set1 = new HashSet<int>() { 1, 2, 3 };
+            var set2 = new HashSet<int>() { 2, 3, 4 };
+
+            set1.SymmetricExceptWith(set2);
+            Assert.IsTrue(set1.SetEquals(new[] { 1, 4 }));
+        }
+
+        [TestMethod]
+        public void RemoveUsuwanie()
+        {
+            var set1 = new HashSet<int>() { 1, 2, 3 };
+            var set2 = new HashSet<int>() { 2, 3, 4 };
+
+            set1.Remove(1);
+
+            Assert.AreEqual(2, set1.Count);
+        }
+
+        [TestMethod]
+        public void RemoveWhereUsuwanie()
+        {
+            var set1 = new HashSet<int>() { 1, 2, 3 };
+            var set2 = new HashSet<int>() { 2, 3, 4 };
+
+            set1.RemoveWhere(x=> x > 1);
+
+            Assert.AreEqual(1, set1.Count);
+        }
+
+        [TestMethod]
+        public void ContainsSprawdzenieCzyJest()
+        {
+            var set1 = new HashSet<int>() { 1, 2, 3 };
+
+
+            set1.Contains(3);
+
+            Assert.IsTrue(set1.Contains(3));
+        }
+    }
+}
