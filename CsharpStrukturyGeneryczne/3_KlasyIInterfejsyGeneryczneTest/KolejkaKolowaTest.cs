50500d3fcd4d39197c5203cdc9146e76553148dd
Przemek O <przemyslaw.oglaza@outlook.com>
2019-08-22 12:19:56 +0200

    Poprawka KolejkiKolowej - za pomoca dziedziczenia oraz testy jednostkowe

---
+++ CsharpStrukturyGeneryczne/3_KlasyIInterfejsyGeneryczneTest/KolejkaKolowaTest.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using _3_KlasyIInterfejsyGeneryczne;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _3_KlasyIInterfejsyGeneryczneTest
+{
+    [TestClass]
+    public class KolejkaKolowaTest
+    {
+        [TestMethod]
+        public void NowaKolejkaJestPusta()
+        {
+            var kolejka = new KolejkaKolowa<double>();
+
+            Assert.IsTrue(kolejka.JestPusty);            
+        }
+
+        [TestMethod]
+        public void KolejkaTrzyelementowaJestPelnaPoTrzechZapisach()
+        {
+            var kolejka = new KolejkaKolowa<double>(pojemnosc: 3);
+            kolejka.Zapisz(3.8);
+            kolejka.Zapisz(5.7);
+            kolejka.Zapisz(9.3);
+
+            Assert.IsTrue(kolejka.JestPelny);
+        }
+
+        [TestMethod]
+        public void PierwszyWchodziPierwszyWychodzi()
+        {
+            var kolejka = new KolejkaKolowa<string>(pojemnosc: 3);
+
+            var wartosc1 = "4.6";
+            var wartosc2 = "3.7";
+
+            kolejka.Zapisz(wartosc1);
+            kolejka.Zapisz(wartosc2);
+
+            Assert.AreEqual(wartosc1, kolejka.Czytaj());
+            Assert.AreEqual(wartosc2, kolejka.Czytaj());
+            Assert.IsTrue(kolejka.JestPusty);
+        }
+
+        [TestMethod]
+        public void NadpisujeGdyJestWiekszaNizPojemnosc()
+        {
+            var kolejka = new KolejkaKolowa<double>(pojemnosc: 3);
+
+            var wartosci = new[] { 1.2, 2.4, 4.3, 7.2, 9.9, 8.5 };
+
+            foreach (var wartosc in wartosci)
+            {
+                kolejka.Zapisz(wartosc);
+            }
+
+            Assert.IsTrue(kolejka.JestPelny);
+            Assert.AreEqual(wartosci[3], kolejka.Czytaj());
+            Assert.AreEqual(wartosci[4], kolejka.Czytaj());
+            Assert.AreEqual(wartosci[5], kolejka.Czytaj());
+            Assert.IsTrue(kolejka.JestPusty);
+        }
+    }
+}
--- CsharpStrukturyGeneryczne/KlasyIInterfejsyGeneryczne/KolejkaKolowa.cs
+++ CsharpStrukturyGeneryczne/KlasyIInterfejsyGeneryczne/KolejkaKolowa.cs
@@ -3,60 +3,29 @@ namespace _3_KlasyIInterfejsyGeneryczne
 {
 
 
-    public class KolejkaKolowa<T> : IKolejka<T>
+    public class KolejkaKolowa<T> : DuzaKolejka<T>
     {
-        private T[] bufor;
-        private int poczatekBufora;
-        private int koniecBufora;
-
-        public KolejkaKolowa() : this(pojemnosc: 5)
+        private int _pojemnosc;
+        public KolejkaKolowa(int pojemnosc = 5)
         {
-
+            _pojemnosc = pojemnosc; 
         }
 
-        public KolejkaKolowa(int pojemnosc)
+        public override void Zapisz(T wartosc)
         {
-            bufor = new T[pojemnosc + 1];
-            poczatekBufora = 0;
-            koniecBufora = 0;
-        }
+            base.Zapisz(wartosc);
 
-        public void Zapisz(T wartosc)
-        {
-            bufor[koniecBufora] = wartosc;
-            koniecBufora = (koniecBufora + 1) % bufor.Length;
-
-            if (koniecBufora == poczatekBufora)
+            if (kolejka.Count > _pojemnosc)
             {
-                poczatekBufora = (poczatekBufora + 1) % bufor.Length;
+                kolejka.Dequeue();
             }
         }
-        public T Czytaj()
-        {
-            var wynik = bufor[poczatekBufora];
-            poczatekBufora = (poczatekBufora + 1) % bufor.Length;
-            return wynik;
-        }
 
-        public int Pojemnosc
+        public override bool JestPelny
         {
             get
             {
-                return bufor.Length;
-            }
-        }
-        public bool JestPusty
-        {
-            get
-            {
-                return koniecBufora == poczatekBufora;
-            }
-        }
-        public bool JestPelny
-        {
-            get
-            {
-                return (koniecBufora + 1) % bufor.Length == poczatekBufora;
+                return kolejka.Count == _pojemnosc;
             }
         }
     }
