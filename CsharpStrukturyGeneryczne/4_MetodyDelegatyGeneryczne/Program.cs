0271db74da9e1956989d2e31079fa842c90c7680
Przemek O <przemyslaw.oglaza@outlook.com>
2019-08-23 12:59:16 +0200

    Delegat Converter i zamiana double na DateTime

--- CsharpStrukturyGeneryczne/4_MetodyDelegatyGeneryczne/Program.cs
+++ CsharpStrukturyGeneryczne/4_MetodyDelegatyGeneryczne/Program.cs
@@ -9,33 +9,53 @@ namespace _4_MetodyDelegatyGeneryczne
 
         static void Main(string[] args)
         {
+            //Predicate();
+            //Func();
+            //Action();
+
+            
+            var kolejka = new KolejkaKolowa<double>();
+
+            WprowadzanieDanych(kolejka);
+
+            var jakoData = kolejka.Mapuj(d => new DateTime(2018, 1, 1).AddDays(d));
+
+            foreach (var item in jakoData)
+            {
+                Console.WriteLine(item); 
+            }
+
+            kolejka.Drukuj(d => Console.WriteLine(d));
+
+            PrzetwarzanieDanych(kolejka);
+        }
+
+        private static void Action()
+        {
+            Action<double> drukuj = x => Console.WriteLine(x);
+            drukuj(7.6);
+            Action<int, int, int, int> test = (a, b, c, d) => Console.WriteLine(a + b + c + d);
+            test(1, 2, 3, 4);
+        }
+
+        private static void Func()
+        {
+            Action<double> drukuj = x => Console.WriteLine(x);
+            Func<double, double> potegowanie = d => d * d;
+            Func<double, double, double> dodaj = (x, y) => x + y;
+
+            drukuj(potegowanie(5));
+            drukuj(dodaj(10, 20));
+        }
+
+        private static void Predicate()
+        {
             Action<bool> drukuj = x => Console.WriteLine(x);
             Func<double, double> potegowanie = d => d * d;
             Func<double, double, double> dodaj = (x, y) => x + y;
             Predicate<double> jestWiekszeOdSto = d => d < 100;
 
-            drukuj(jestWiekszeOdSto(potegowanie(dodaj(6,8))));
-
-            //Action<double> drukuj = x => Console.WriteLine(x);
-            //Func<double, double> potegowanie = d => d * d;
-            //Func<double, double, double> dodaj = (x, y) => x + y;
-
-            //drukuj(potegowanie(5));
-            //drukuj(dodaj(10, 20));
-
-
-            //Action<double> drukuj = x => Console.WriteLine(x);
-            //drukuj(7.6);
-            //Action<int, int, int, int> test = (a, b, c, d) => Console.WriteLine(a+b+c+d);
-            //test(1, 2, 3, 4);
-
-            var kolejka = new KolejkaKolowa<double>();
-
-            WprowadzanieDanych(kolejka);
-
-            kolejka.Drukuj(d => Console.WriteLine(d));
- 
-            PrzetwarzanieDanych(kolejka);
+            drukuj(jestWiekszeOdSto(potegowanie(dodaj(6, 8))));
         }
 
         private static void PrzetwarzanieDanych(KolejkaKolowa<double> kolejka)
