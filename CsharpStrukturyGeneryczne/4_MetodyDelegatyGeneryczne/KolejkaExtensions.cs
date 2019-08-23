b087f514bf54482bfb5bdc24441cfc4ffa24cf6a
Przemek O <przemyslaw.oglaza@outlook.com>
2019-08-23 11:35:33 +0200

    Delegaty generyczne - drukarka

--- CsharpStrukturyGeneryczne/4_MetodyDelegatyGeneryczne/KolejkaExtensions.cs
+++ CsharpStrukturyGeneryczne/4_MetodyDelegatyGeneryczne/KolejkaExtensions.cs
@@ -4,8 +4,11 @@ using System.ComponentModel;
 
 namespace _4_MetodyDelegatyGeneryczne
 {
+    public delegate void Drukarka<T>(T dane);
+
     public static class KolejkaExtensions
-    {
+
+    {        
         public static IEnumerable<Twyjscie> ElementJako<T, Twyjscie>(this IKolejka<T> kolejka) 
         {
             var konwerter = TypeDescriptor.GetConverter(typeof(T));
@@ -17,11 +20,11 @@ namespace _4_MetodyDelegatyGeneryczne
             }
         }
 
-        public static void Drukuj<T>(this IKolejka<T> kolejka)
+        public static void Drukuj<T>(this IKolejka<T> kolejka, Drukarka<T> wydruk)
         {
             foreach (var item in kolejka)
             {
-                System.Console.WriteLine(item);
+                wydruk(item);
             }
         }
         
