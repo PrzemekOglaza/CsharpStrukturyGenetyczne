384a342c9d915ad73b79067fe046f1b4de892622
Przemek O <przemyslaw.oglaza@outlook.com>
2019-08-23 10:50:26 +0200

    Zamina elementu z double  na int, string

---
+++ CsharpStrukturyGeneryczne/4_MetodyDelegatyGeneryczne/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+
+namespace _4_MetodyDelegatyGeneryczne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var kolejka = new KolejkaKolowa<double>();
+
+            WprowadzanieDanych(kolejka);
+
+            var elementyJakoInt = kolejka.ElementJako<int>();
+                
+            foreach (var item in elementyJakoInt)
+            {
+                Console.WriteLine(item);
+            }
+            PrzetwarzanieDanych(kolejka);
+        }
+
+        private static void PrzetwarzanieDanych(KolejkaKolowa<double> kolejka)
+        {
+            var suma = 0.0;
+            Console.WriteLine("W naszej kolejce jest: ");
+
+            while (!kolejka.JestPusty)
+            {
+                {
+                    suma += kolejka.Czytaj();
+                }
+            }
+
+            Console.WriteLine(suma);
+        }
+
+        private static void WprowadzanieDanych(KolejkaKolowa<double> kolejka)
+        {
+            while (true)
+            {
+                var wartosc = 0.0;
+
+                var wartoscWejsciowa = Console.ReadLine();
+
+                if (double.TryParse(wartoscWejsciowa, out wartosc))
+                {
+                    kolejka.Zapisz(wartosc);
+                    continue;
+                }
+                break;
+            }
+        }
+    }
+}
